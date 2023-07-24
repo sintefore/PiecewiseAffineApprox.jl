d748d442293e5118590ba107fe0be2186dc79359
Lars Hellemo <Lars.Hellemo@sintef.no>
2023-07-24 16:34:17 +0200

    Formatting

--- test/test_2D_convexapprox.jl
+++ test/test_2D_convexapprox.jl
@@ -1,59 +1,19 @@
 @testset "2D Convex Approx" begin
-xg = [i for i ∈ -1:0.5:1]
-yg = [j for j ∈ -1:0.5:1]
+    xg = [i for i ∈ -1:0.5:1]
+    yg = [j for j ∈ -1:0.5:1]
 
-X = [repeat(xg, inner = [size(yg, 1)]) repeat(yg, outer = [size(xg, 1)])]
+    X = [repeat(xg, inner = [size(yg, 1)]) repeat(yg, outer = [size(xg, 1)])]
 
-z = X[:, 1] .^ 2 + X[:, 2] .^ 2
-z_concave = z .* -1
+    z = X[:, 1] .^ 2 + X[:, 2] .^ 2
+    z_concave = z .* -1
 
-np = 17
+    np = 17
 
-function mat2tuples(x::Matrix)
-    return collect(Tuple(x'[:, i]) for i ∈ 1:size(x', 2))
-end
+    function mat2tuples(x::Matrix)
+        return collect(Tuple(x'[:, i]) for i ∈ 1:size(x', 2))
+    end
 
-pwl1 = approx(
-    FunctionEvaluations(mat2tuples(X), z),
-    Convex(),
-    Optimized();
-    optimizer = optimizer,
-    planes = np,
-    dimensions = 2,
-    strict = :above,
-    pen = :l1,
-)
-pwl2 = approx(
-    FunctionEvaluations(mat2tuples(X), z_concave),
-    Concave(),
-    Optimized();
-    optimizer = optimizer,
-    planes = np,
-    dimensions = 2,
-    strict = :above,
-    pen = :l1,
-)
-
-# @test length(pwl1.a) == np
-@test isapprox(PWA.evaluate(pwl1, (0.5, 0.5)), 0.5, atol = 0.1)
-@test isapprox(
-    PWA.evaluate(pwl1, (-0.3, 0.4)),
-    -PWA.evaluate(pwl2, (-0.3, 0.4)),
-    atol = 0.01,
-)
-
-# Test with constraints added using already existing variables (tuple with xvar and yvar)
-m = Model()
-@variable(m, xvar)
-@variable(m, yvar)
-@variable(m, test_f)
-
-tuple_var = (xvar, yvar)
-
-y = PWA.pwlinear(
-    m,
-    tuple_var,
-    approx(
+    pwl1 = approx(
         FunctionEvaluations(mat2tuples(X), z),
         Convex(),
         Optimized();
@@ -62,54 +22,94 @@ y = PWA.pwlinear(
         dimensions = 2,
         strict = :above,
         pen = :l1,
-    );
-    z = test_f,
-)
+    )
+    pwl2 = approx(
+        FunctionEvaluations(mat2tuples(X), z_concave),
+        Concave(),
+        Optimized();
+        optimizer = optimizer,
+        planes = np,
+        dimensions = 2,
+        strict = :above,
+        pen = :l1,
+    )
 
-@objective(m, Min, y)
-set_optimizer(m, optimizer)
-@constraint(m, xvar == √0.5)
-@constraint(m, yvar == √0.5)
-optimize!(m)
+    # @test length(pwl1.a) == np
+    @test isapprox(PWA.evaluate(pwl1, (0.5, 0.5)), 0.5, atol = 0.1)
+    @test isapprox(
+        PWA.evaluate(pwl1, (-0.3, 0.4)),
+        -PWA.evaluate(pwl2, (-0.3, 0.4)),
+        atol = 0.01,
+    )
 
-xval = JuMP.value(m[:xvar])
-yval = JuMP.value(m[:yvar])
-fval = JuMP.value(m[:test_f])
+    # Test with constraints added using already existing variables (tuple with xvar and yvar)
+    m = Model()
+    @variable(m, xvar)
+    @variable(m, yvar)
+    @variable(m, test_f)
 
-@test isapprox(value(m[:test_f]), 1.0, rtol = 0.12)
+    tuple_var = (xvar, yvar)
 
-m = Model()
-@variable(m, xvar_conc)
-@variable(m, yvar_conc)
-@variable(m, f_conc)
+    y = PWA.pwlinear(
+        m,
+        tuple_var,
+        approx(
+            FunctionEvaluations(mat2tuples(X), z),
+            Convex(),
+            Optimized();
+            optimizer = optimizer,
+            planes = np,
+            dimensions = 2,
+            strict = :above,
+            pen = :l1,
+        );
+        z = test_f,
+    )
 
-tuple_var_conc = (xvar_conc, yvar_conc)
+    @objective(m, Min, y)
+    set_optimizer(m, optimizer)
+    @constraint(m, xvar == √0.5)
+    @constraint(m, yvar == √0.5)
+    optimize!(m)
 
-y_concave = PWA.pwlinear(
-    m,
-    tuple_var_conc,
-    FunctionEvaluations(mat2tuples(X), z_concave),
-    Concave(),
-    Optimized();
-    optimizer = optimizer,
-    planes = np,
-    dimensions = 2,
-    strict = :above,
-    pen = :l1,
-    z = f_conc,
-)
+    xval = JuMP.value(m[:xvar])
+    yval = JuMP.value(m[:yvar])
+    fval = JuMP.value(m[:test_f])
 
-@objective(m, Max, y_concave)
-set_optimizer(m, optimizer)
-@constraint(m, xvar_conc == √0.5)
-@constraint(m, yvar_conc == √0.5)
-optimize!(m)
+    @test isapprox(value(m[:test_f]), 1.0, rtol = 0.12)
 
-xval_conc = JuMP.value(m[:xvar_conc])
-yval_conc = JuMP.value(m[:yvar_conc])
-fval_conc = JuMP.value(m[:f_conc])
+    m = Model()
+    @variable(m, xvar_conc)
+    @variable(m, yvar_conc)
+    @variable(m, f_conc)
 
-@test isapprox(fval, -fval_conc, atol = 0.01)
-@test isapprox(xval, xval_conc, atol = 0.01)
-@test isapprox(yval, yval_conc, atol = 0.01)
+    tuple_var_conc = (xvar_conc, yvar_conc)
+
+    y_concave = PWA.pwlinear(
+        m,
+        tuple_var_conc,
+        FunctionEvaluations(mat2tuples(X), z_concave),
+        Concave(),
+        Optimized();
+        optimizer = optimizer,
+        planes = np,
+        dimensions = 2,
+        strict = :above,
+        pen = :l1,
+        z = f_conc,
+    )
+
+    @objective(m, Max, y_concave)
+    set_optimizer(m, optimizer)
+    @constraint(m, xvar_conc == √0.5)
+    @constraint(m, yvar_conc == √0.5)
+    optimize!(m)
+
+    xval_conc = JuMP.value(m[:xvar_conc])
+    yval_conc = JuMP.value(m[:yvar_conc])
+    fval_conc = JuMP.value(m[:f_conc])
+
+    @test isapprox(fval, -fval_conc, atol = 0.01)
+    @test isapprox(xval, xval_conc, atol = 0.01)
+    @test isapprox(yval, yval_conc, atol = 0.01)
 end
--- test/testquadapprox.jl
+++ test/testquadapprox.jl
@@ -1,84 +1,84 @@
 @testset "Quad Approx" begin
-# Points to approximate
-points = [i => i^2 for i ∈ 0:0.1:1]
+    # Points to approximate
+    points = [i => i^2 for i ∈ 0:0.1:1]
 
-@testset "L1" begin
-# Test with constraints added to existing model
-m = Model()
-@variable(m, x)
-y = PWA.pwlinear(
-    m,
-    tuple(x),
-    FunctionEvaluations(
-        [tuple(i.first) for i ∈ points],
-        [i.second for i ∈ points],
-    ),
-    Convex(),
-    Optimized();
-    optimizer,
-    pen = :l1,
-    planes = 5,
-)
-@objective(m, Min, y)
-set_optimizer(m, optimizer)
-@constraint(m, x >= 0.3)
-optimize!(m)
-@test isapprox(value(y), 0.09, atol = 0.015)
-@constraint(m, x >= 0.9)
-optimize!(m)
-@test isapprox(value(y), 0.81, atol = 0.1)
+    @testset "L1" begin
+        # Test with constraints added to existing model
+        m = Model()
+        @variable(m, x)
+        y = PWA.pwlinear(
+            m,
+            tuple(x),
+            FunctionEvaluations(
+                [tuple(i.first) for i ∈ points],
+                [i.second for i ∈ points],
+            ),
+            Convex(),
+            Optimized();
+            optimizer,
+            pen = :l1,
+            planes = 5,
+        )
+        @objective(m, Min, y)
+        set_optimizer(m, optimizer)
+        @constraint(m, x >= 0.3)
+        optimize!(m)
+        @test isapprox(value(y), 0.09, atol = 0.015)
+        @constraint(m, x >= 0.9)
+        optimize!(m)
+        @test isapprox(value(y), 0.81, atol = 0.1)
 
-# Test with constraints added using already existing y-variable
-m = Model()
-@variable(m, x)
-@variable(m, test_y)
-y = PWA.pwlinear(
-    m,
-    tuple(x),
-    FunctionEvaluations(
-        [tuple(i.first) for i ∈ points],
-        [i.second for i ∈ points],
-    ),
-    Convex(),
-    Optimized();
-    optimizer,
-    pen = :l1,
-    planes = 5,
-    z = test_y,
-)
-@objective(m, Min, y)
-set_optimizer(m, optimizer)
-@constraint(m, x >= 0.3)
-optimize!(m)
-@test isapprox(value(m[:test_y]), 0.09, atol = 0.015)
-end
+        # Test with constraints added using already existing y-variable
+        m = Model()
+        @variable(m, x)
+        @variable(m, test_y)
+        y = PWA.pwlinear(
+            m,
+            tuple(x),
+            FunctionEvaluations(
+                [tuple(i.first) for i ∈ points],
+                [i.second for i ∈ points],
+            ),
+            Convex(),
+            Optimized();
+            optimizer,
+            pen = :l1,
+            planes = 5,
+            z = test_y,
+        )
+        @objective(m, Min, y)
+        set_optimizer(m, optimizer)
+        @constraint(m, x >= 0.3)
+        optimize!(m)
+        @test isapprox(value(m[:test_y]), 0.09, atol = 0.015)
+    end
 
-@testset "L2" begin
-   # Test with constraints added to existing model
-   for met in [Heuristic(),Optimized()]#(Optimized(), Heuristic())
-m = Model()
-@variable(m, x)
-y = PWA.pwlinear(
-    m,
-    tuple(x),
-    FunctionEvaluations(
-        [tuple(i.first) for i ∈ points],
-        [i.second for i ∈ points],
-    ),
-    Convex(),
-    met;
-    optimizer=qp_optimizer,
-    pen = :l2,
-    planes = 5,
-)
-@objective(m, Min, y)
-    set_optimizer(m, optimizer)
-@constraint(m, x >= 0.3)
-optimize!(m)
-@test isapprox(value(y), 0.09, atol = 0.015)
-@constraint(m, x >= 0.9)
-optimize!(m)
-@test isapprox(value(y), 0.81, atol = 0.1)
-end
-end
+    @testset "L2" begin
+        # Test with constraints added to existing model
+        for met in [Heuristic(), Optimized()]
+            m = Model()
+            @variable(m, x)
+            y = PWA.pwlinear(
+                m,
+                tuple(x),
+                FunctionEvaluations(
+                    [tuple(i.first) for i ∈ points],
+                    [i.second for i ∈ points],
+                ),
+                Convex(),
+                met;
+                optimizer = qp_optimizer,
+                pen = :l2,
+                planes = 5,
+            )
+            @objective(m, Min, y)
+            set_optimizer(m, optimizer)
+            @constraint(m, x >= 0.3)
+            optimize!(m)
+            @test isapprox(value(y), 0.09, atol = 0.015)
+            @constraint(m, x >= 0.9)
+            optimize!(m)
+            @test isapprox(value(y), 0.81, atol = 0.1)
+        end
+    end
 end
