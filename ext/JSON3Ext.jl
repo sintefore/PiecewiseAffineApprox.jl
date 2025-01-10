8cf9349373a89d2c8080b4175c33ab6b71eb8e47
Lars Hellemo <Lars.Hellemo@sintef.no>
2025-01-10 10:27:31 +0100

    Formatting

--- ext/JSON3Ext.jl
+++ ext/JSON3Ext.jl
@@ -4,8 +4,11 @@ using PiecewiseAffineApprox
 using JSON3
 
 # Overload read/write of JSON3 to use ST-compatible intermediate representation
-JSON3.write(fn::String, pwaf::PWAFunc) = JSON3.write(fn, PiecewiseAffineApprox.STFunc(pwaf))
-JSON3.read(s::AbstractString, ::Type{PWAFunc}) = PWAFunc(JSON3.read(s, PiecewiseAffineApprox.STFunc))
-
+function JSON3.write(fn::String, pwaf::PWAFunc)
+    return JSON3.write(fn, PiecewiseAffineApprox.STFunc(pwaf))
+end
+function JSON3.read(s::AbstractString, ::Type{PWAFunc})
+    return PWAFunc(JSON3.read(s, PiecewiseAffineApprox.STFunc))
+end
 
 end
