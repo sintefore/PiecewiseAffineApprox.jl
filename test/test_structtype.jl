56e12a9f207d4d56e5e02c0184daf1f839f81850
hellemo <lars.hellemo@sintef.no>
2025-10-17 09:50:05 +0200

    Serialization with JSON v1.0 (#27)
    Add JSON extension to (de)serialize with curvature as for JSON3.

--- test/test_structtype.jl
+++ test/test_structtype.jl
@@ -1,21 +1,44 @@
-@testset "Serialization" begin
+@testset "JSON3 Serialization" begin
     x = 1:10
     y = x .^ 2
     tmp = mktempdir()
     pwa = PiecewiseAffineApprox.approx(
         FunctionEvaluations(Tuple.(x), y),
         Convex(),
-        MILP(; optimizer = HiGHS.Optimizer, planes = 5),
+        MILP(; optimizer, planes = 5),
     )
     pwa_concave = PiecewiseAffineApprox.approx(
         FunctionEvaluations(Tuple.(x), -y),
         Concave(),
-        MILP(; optimizer = HiGHS.Optimizer, planes = 5),
+        MILP(; optimizer, planes = 5),
     )
     for (i, p) ∈ enumerate((pwa, pwa_concave))
         fn = joinpath(tmp, "test_$i.json")
         JSON3.write(fn, p)
         read_back = JSON3.read(fn, PWAFunc)
+        @test isa(read_back, PWAFunc)
+        @test length(read_back.planes) == 5
+    end
+end
+
+@testset "JSON Serialization" begin
+    x = 1:10
+    y = x .^ 2
+    tmp = mktempdir()
+    pwa = PiecewiseAffineApprox.approx(
+        FunctionEvaluations(Tuple.(x), y),
+        Convex(),
+        MILP(; optimizer, planes = 5),
+    )
+    pwa_concave = PiecewiseAffineApprox.approx(
+        FunctionEvaluations(Tuple.(x), -y),
+        Concave(),
+        MILP(; optimizer, planes = 5),
+    )
+    for (i, p) ∈ enumerate((pwa, pwa_concave))
+        fn = joinpath(tmp, "test_$i.json")
+        JSON.json(fn, p)
+        read_back = JSON.parsefile(fn, PWAFunc)
         @test isa(read_back, PWAFunc)
         @test length(read_back.planes) == 5
     end
